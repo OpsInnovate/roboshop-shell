8e5ac84f4a84826e08ca9031c16cf73cb1c00065
karthik <karthik.patel06@gmail.com>
2024-07-06 23:40:50 +0530

    modified catalogue.sh file

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,8 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-cp catalogue.service /etc/systemd/system/catalogue.service
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
@@ -12,3 +13,5 @@ npm install
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
+dnf install mongodb-mongosh -y
+mongosh --host localhost </app/db/master-data.js
