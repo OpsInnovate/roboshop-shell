0eacde74a34dfa3c51ac00bd04e6330ebb44f35a
karthik <karthik.patel06@gmail.com>
2024-07-07 00:23:02 +0530

    added user.sh file

---
+++ user.sh
@@ -0,0 +1,13 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+cd /app
+unzip /tmp/user.zip
+npm install
+#restart service
+systemctl daemon-reload
+systemctl enable user
+systemctl restart user
